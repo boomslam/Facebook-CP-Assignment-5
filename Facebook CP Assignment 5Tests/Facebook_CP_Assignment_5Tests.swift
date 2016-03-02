bebcf36c06974704470248083dcbf9e8264e6401
Jess <jesswlam@gmail.com>
2016-03-02 09:01:48 -0800

    Initial Commit

---
+++ Facebook CP Assignment 5Tests/Facebook_CP_Assignment_5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Facebook_CP_Assignment_5Tests.swift
+//  Facebook CP Assignment 5Tests
+//
+//  Created by Jess Lam on 3/2/16.
+//  Copyright © 2016 Jess Lam. All rights reserved.
+//
+
+import XCTest
+@testable import Facebook_CP_Assignment_5
+
+class Facebook_CP_Assignment_5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
